f2399890f7a6cd662ecdebc7f787aad26b30dce6
dungne1104 <phanthuydung6666@gmail.com>
2022-08-29 13:38:07 +0700

    update code chuan HD1

--- HD1/HD1.ino
+++ HD1/HD1.ino
@@ -7,6 +7,98 @@
 #define led6 4
 #define led7 5
 
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+
+
 int const button[] = {6, 7, 8, 9, 10, 11, 12};
 bool danhdau[8];
 int test[8];
@@ -80,10 +172,19 @@ void checkvong2(int n){
     delay(1000);
     Tat(test[v]); Tat(test[v+1]);
     for (int i=0;i<=v;i+=2){
-      while (digitalRead(button[test[i]-1]) == 1 && digitalRead(button[test[i+1]-1]) == 1){
+      //1 la tat, 0 la bat
+      while (digitalRead(button[test[i]-1]) == 1 || digitalRead(button[test[i+1]-1]) == 1){
         for (int j=0;j<n;j++) {
-          if (j!=test[i]-1 && j!=test[i+1]-1 && digitalRead(button[j])==0) gameover();
-        }
+          if (j!=test[i]-1 && j!=test[i+1]-1 && digitalRead(button[j])==0)gameover();
+          if (digitalRead(button[test[i]-1]) == 1 && digitalRead(button[test[i+1]-1]) == 0) {
+            delay(100);
+            if (digitalRead(button[test[i]-1]) == 1) gameover();
+          }
+          if  (digitalRead(button[test[i]-1]) == 0 && digitalRead(button[test[i+1]-1]) == 1){
+            delay(100);
+            if (digitalRead(button[test[i+1]-1]) == 1) gameover();
+          }
+        } 
       } Bat(test[i]);  Bat(test[i+1]);delay(500);
     } delay(500);
     for (int i=0;i<=v;i+=2) Tat(test[i]),Tat(test[i+1]);
@@ -92,12 +193,13 @@ void checkvong2(int n){
 }
 
 void checkvong3(int n){
-  int tam=random(1,2),v=0,dem=0;
+  int tam=random(1,1002)  % 2 + 1,v=0,dem=0;
   while (v<n){
-    int temp=0,i=0;
-    tam=random(1,2); Bat(test[v]);
+    int temp=0,i=0; 
+    int tam=random(1,1000) % 2 + 1; Bat(test[v]);
     if (tam==2) Bat(test[v+1]); 
     delay(1000); Tat(test[v]); Tat(test[v+1]);
+    vong3[dem]=tam;
     while (i<=v){
       if (vong3[temp]==1){
         while (digitalRead(button[test[i]-1]) == 1){
@@ -106,16 +208,24 @@ void checkvong3(int n){
           }
         }  Bat(test[i]); delay(500);
       } else {
-        while (digitalRead(button[test[i]-1]) == 1 && digitalRead(button[test[i+1]-1]) == 1){
+        while (digitalRead(button[test[i]-1]) == 1 || digitalRead(button[test[i+1]-1]) == 1){
           for (int j=0;j<n;j++) {
-            if (j!=test[i]-1 && j!=test[i+1]-1 && digitalRead(button[j])==0) gameover();
-          }
-        } Bat(test[i]);  Bat(test[i+1]); delay(500);
+            if (j!=test[i]-1 && j!=test[i+1]-1 && digitalRead(button[j])==0)gameover();
+            if (digitalRead(button[test[i]-1]) == 1 && digitalRead(button[test[i+1]-1]) == 0) {
+              delay(100);
+              if (digitalRead(button[test[i]-1]) == 1) gameover();
+            }
+            if  (digitalRead(button[test[i]-1]) == 0 && digitalRead(button[test[i+1]-1]) == 1){
+              delay(100);
+              if (digitalRead(button[test[i+1]-1]) == 1) gameover();
+            }
+          } 
+        }Bat(test[i]);  Bat(test[i+1]);delay(500);
       }i+=vong3[temp]; temp++;
     } delay(500);
     for (int i=0;i<=n;i++) Tat(test[i]);
     delay(500);
-    v+=tam; vong3[dem]=tam; dem++;
+    v+=tam;  dem++;
   }
 }
 
@@ -132,15 +242,93 @@ void gameover() {
   } delay(2000);
 }
 
+void superidol(){
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_DS6, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_C6, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_G5, 400);
+  delay(400);
+  tone(A2, NOTE_C6, 200);
+  delay(200);
+  tone(A2, NOTE_AS5, 200);
+  delay(200);
+  tone(A2, NOTE_G5, 400);
+  delay(400);
+  tone(A2, NOTE_AS5, 400);
+  delay(400);
+  tone(A2, NOTE_C6, 400);
+  delay(400);
+  tone(A2, NOTE_C6, 200);
+  delay(200);
+  tone(A2, NOTE_C6, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_C6, 200);
+  delay(200);
+  tone(A2, NOTE_AS5, 200);
+  delay(200);
+  tone(A2, NOTE_C6, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 400);
+  delay(400);
+  tone(A2, NOTE_AS5, 200);
+  delay(200);
+  tone(A2, NOTE_G5, 200);
+  delay(200);
+  tone(A2, NOTE_G5, 200);
+  delay(200);
+  tone(A2, NOTE_AS5, 200);
+  delay(200);
+  tone(A2, NOTE_G5, 200);
+  delay(200);
+  tone(A2, NOTE_AS5, 200);
+  delay(200);
+  tone(A2, NOTE_G5, 400);
+  delay(400);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_DS6, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_C6, 200);
+  delay(200);
+  tone(A2, NOTE_AS5, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_C6, 200);
+  delay(200);
+  tone(A2, NOTE_D6, 200);
+  delay(200);
+  tone(A2, NOTE_G5, 400);
+  delay(400);
+}
 
 void loop() {
   RandomTest(7);
   checkvong1(4);
-  delay(2000);
+  tone(A2, 988, 300); 
+  delay(1000);
   RandomTest(7);
-  checkvong2(7);
+  checkvong2(6);
   delay(2000);
+  tone(A2, 988, 300); 
+  delay(1000);
   RandomTest(7);
   checkvong3(7);
+  superidol();
   delay(10000000);
 }
